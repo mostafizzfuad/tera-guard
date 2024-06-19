508180e3781b6a08e380c6fb0c5eb0460df18efd
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-06-19 12:59:56 +0600

    modal window and smooth scrolling added

--- script.js
+++ script.js
@@ -62,3 +62,42 @@ allSections.forEach((section) => {
     sectionObserver.observe(section);
     section.classList.add("section--hidden");
 });
+
+
+/*----------------- modal window -----------------*/
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+
+/*----------------- smooth scrolling -----------------*/
+// page navigation
+navLinks.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href");
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+});
+
+// button scrolling
+btnScrollTo.addEventListener("click", () => {
+    section1.scrollIntoView({ behavior: "smooth" });
+});
